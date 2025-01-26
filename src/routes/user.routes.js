37896f083867723978a9978c7e12b7193cd53e1c
satya prakash <satya200prakash@gmail.com>
2025-01-26 16:46:48 +0530

    miss match folder name

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import { registerUser, loginUser, logoutUser } from "../controllers/user.controller.js";
 
 import { upload } from "../middlewares/multer.middlewares.js";
-import { verifyJWT } from "../middleware/auth.midddleware.js"
+import {verifyJWT} from "../middlewares/auth.middlewares.js";
 import { ApiError } from "../utils/ApiError.js";
 
 const router = Router()
